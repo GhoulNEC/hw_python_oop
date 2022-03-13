f50e63e997f6085feb85c44205ab6ae3de0a3358
Роман Евстафьев <ev-rog96@mail.ru>
2022-03-13 15:30:17 +0300

    fix errors

--- homework.py
+++ homework.py
@@ -61,8 +61,8 @@ class Running(Training):
         coeff_calorie_1 = 18
         coeff_calorie_2 = 20
         return ((coeff_calorie_1 * self.get_mean_speed() - coeff_calorie_2)
-                 * self.weight / self.M_IN_KM
-                 * (self.duration * self.MIN_IN_H))
+                * self.weight / self.M_IN_KM
+                * (self.duration * self.MIN_IN_H))
 
 
 class SportsWalking(Training):
@@ -81,7 +81,7 @@ class SportsWalking(Training):
         coeff_calorie_2 = 0.029
         return (
             (coeff_calorie_1 * self.weight
-            + (self.get_mean_speed()**2 // self.weight)
+             + (self.get_mean_speed()**2 // self.weight)
             * coeff_calorie_2 * self.weight)
             * (self.duration * self.MIN_IN_H))
 
@@ -102,7 +102,7 @@ class Swimming(Training):
         self.count_pool = count_pool
 
     def get_mean_speed(self) -> float:
-        return (self.length_pool * self.count_pool / self.M_IN_KM 
+        return (self.length_pool * self.count_pool / self.M_IN_KM
                 / self.duration)
 
     def get_spent_calories(self) -> float:
